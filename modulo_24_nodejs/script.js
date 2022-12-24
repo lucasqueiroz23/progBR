94dcae4aae6966b0678349a8a8ed72a29bf45503
Lucas <lucas.fn4@gmail.com>
2022-12-24 14:04:17 -0300

    remoção de estudos feitos anteriormente. a remoção foi feita para que eu estude o módulo novamente, com mais calma, no futuro.

--- modulo_24_nodejs/script.js
+++
@@ -1,7 +0,0 @@
-// estou importanto o objeto 'exemplo'
-const exemplo = require('./modulo_exemplo');
-
-const args = process.argv.slice(2);
-
-console.log('Seu nome é ');
-console.log(exemplo.printaNomeCompleto(args));
